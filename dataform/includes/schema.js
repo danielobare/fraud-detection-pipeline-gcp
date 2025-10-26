de818e61b61889e57a4b596e1732e7f7f4bb22de
Daniel Obare Nyakundi <dnobare@gmail.com>
2025-10-27 02:03:21 +0300

    modified schema.js and dataform.json

--- dataform/includes/schema.js
+++ dataform/includes/schema.js
@@ -1,11 +1,4 @@
-// This file defines common schema & reusable helpers across all SQLX files
-
-// Reference to the source dataset and table
-const sourceDataset = dataform.projectConfig.vars.source_dataset;
-const transactionsTable = dataform.projectConfig.vars.transactions_table;
-
-// Export variables for use in SQLX definitions
 module.exports = {
-  sourceDataset,
-  transactionsTable
+  defaultSchema: "fraud_detection",
+  tags: ["fraud", "transactions", "data_pipeline"]
 };
